a44bd1cb69be0051214d1ab4f40178c1adec01eb
Alan Thomson <alan@projectmccoll.co.uk>
2017-10-10 16:10:19 +0100

    deploy

--- build/3.9b122e362cc4df9391f3.chunk.js
+++ build/3.9b122e362cc4df9391f3.chunk.js
@@ -1,1 +1,114 @@
-webpackJsonp([3],{"./app/components/Print/index.js":function(a,b,c){"use strict";function d(a,b){return Object.freeze(Object.defineProperties(a,{raw:{value:Object.freeze(b)}}))}Object.defineProperty(b,"__esModule",{value:!0});var e=c("./node_modules/react/react.js"),f=c.n(e),g=c("./node_modules/styled-components/dist/styled-components.es.js"),h=c("./app/components/Print/styles.css"),i=c.n(h),j=function(){var a="function"==typeof Symbol&&Symbol.for&&Symbol.for("react.element")||60103;return function(b,c,d,e){var f=b&&b.defaultProps,g=arguments.length-3;if(c||0==g||(c={}),c&&f)for(var h in f)void 0===c[h]&&(c[h]=f[h]);else c||(c=f||{});if(1==g)c.children=e;else if(1<g){for(var j=Array(g),k=0;k<g;k++)j[k]=arguments[k+3];c.children=j}return{$$typeof:a,type:b,key:void 0===d?null:""+d,ref:null,props:c,_owner:null}}}(),k=d(["\n  text-align:center;\n"],["\n  text-align:center;\n"]),l=d(["\n  display:block;\n  text-align:center;\n  background:#5cbfdc;\n  color:#fff;\n  text-decoration:none;\n  border-radius:5px;\n  margin:1em;\n  padding:1em;\n  &:hover {\n    cursor:pointer;\n    background:#085FFF;\n  }\n"],["\n  display:block;\n  text-align:center;\n  background:#5cbfdc;\n  color:#fff;\n  text-decoration:none;\n  border-radius:5px;\n  margin:1em;\n  padding:1em;\n  &:hover {\n    cursor:pointer;\n    background:#085FFF;\n  }\n"]),m=g.a.div(k),n=g.a.a(l),o=j("div",{},void 0,j("div",{className:"no-print"},void 0,j(m,{},void 0,j("img",{src:"/images/cookie-dough-detail.png",alt:"Free Icecream"}),j("br",{}),j("h1",{},void 0,"One free ice cream topping")),j(m,{},void 0,j("img",{src:"/images/barcode.png",alt:"barcode"})),j(n,{onClick:function(){window.print(),setTimeout(function(){window.location.href="/"},100)}},void 0,"Print")),j("div",{className:"token"},void 0,j("div",{className:"logo"},void 0,j("img",{src:"/images/logo.svg",alt:"logo"})),j("p",{},void 0,"Congratulations!! You have beaten our Tomato Trader Challenge. Head over to the Ben & Jerry\u2019s stall for a small peaceful reward with your ice cream and hopefully now you\u2019ll find it easier to negotiate the next challenge you face, like the strong women in the Democratic Republic of Congo"),j(m,{},void 0,j("img",{src:"/images/barcode.png",alt:"barcode"}))));b["default"]=function(){return o}},"./app/components/Print/styles.css":function(a,b,c){var d=c("./node_modules/css-loader/index.js!./app/components/Print/styles.css");"string"==typeof d&&(d=[[a.i,d,""]]);c("./node_modules/style-loader/addStyles.js")(d,{});d.locals&&(a.exports=d.locals),!1},"./node_modules/css-loader/index.js!./app/components/Print/styles.css":function(a,b,c){b=a.exports=c("./node_modules/css-loader/lib/css-base.js")(),b.push([a.i,"\n.token {\n\ttext-align: center;\t\n\tpadding:8px;\n\tdisplay: none;\n}\n\nimg {\n\tmax-width: 90%;\n}\n\n\n@media print {\n  .no-print {\n    display: none;\n\t}\n\t.token {\n\t\tdisplay: block;\n\t}\n}",""])}});
+webpackJsonp([3],{
+
+/***/ "./app/components/Print/index.js":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_styled_components__ = __webpack_require__("./node_modules/styled-components/dist/styled-components.es.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__styles_css__ = __webpack_require__("./app/components/Print/styles.css");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__styles_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__styles_css__);
+var _jsx = function () { var REACT_ELEMENT_TYPE = typeof Symbol === "function" && Symbol.for && Symbol.for("react.element") || 0xeac7; return function createRawReactElement(type, props, key, children) { var defaultProps = type && type.defaultProps; var childrenLength = arguments.length - 3; if (!props && childrenLength !== 0) { props = {}; } if (props && defaultProps) { for (var propName in defaultProps) { if (props[propName] === void 0) { props[propName] = defaultProps[propName]; } } } else if (!props) { props = defaultProps || {}; } if (childrenLength === 1) { props.children = children; } else if (childrenLength > 1) { var childArray = Array(childrenLength); for (var i = 0; i < childrenLength; i++) { childArray[i] = arguments[i + 3]; } props.children = childArray; } return { $$typeof: REACT_ELEMENT_TYPE, type: type, key: key === undefined ? null : '' + key, ref: null, props: props, _owner: null }; }; }();
+
+var _templateObject = _taggedTemplateLiteral(['\n  text-align:center;\n'], ['\n  text-align:center;\n']),
+    _templateObject2 = _taggedTemplateLiteral(['\n  display:block;\n  text-align:center;\n  background:#5cbfdc;\n  color:#fff;\n  text-decoration:none;\n  border-radius:5px;\n  margin:1em;\n  padding:1em;\n  &:hover {\n    cursor:pointer;\n    background:#085FFF;\n  }\n'], ['\n  display:block;\n  text-align:center;\n  background:#5cbfdc;\n  color:#fff;\n  text-decoration:none;\n  border-radius:5px;\n  margin:1em;\n  padding:1em;\n  &:hover {\n    cursor:pointer;\n    background:#085FFF;\n  }\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+/**
+*
+* Print
+*
+*/
+
+
+
+
+
+
+var ImageContainer = __WEBPACK_IMPORTED_MODULE_1_styled_components__["a" /* default */].div(_templateObject);
+var PrintLink = __WEBPACK_IMPORTED_MODULE_1_styled_components__["a" /* default */].a(_templateObject2);
+
+var print = function print() {
+  window.print();
+  setTimeout(function () {
+    window.location.href = '/';
+  }, 100);
+};
+
+var _ref = _jsx('div', {}, void 0, _jsx('div', {
+  className: 'no-print'
+}, void 0, _jsx(ImageContainer, {}, void 0, _jsx('img', {
+  src: '/images/cookie-dough-detail.png',
+  alt: 'Free Icecream'
+}), _jsx('br', {}), _jsx('h1', {}, void 0, 'One free ice cream topping')), _jsx(ImageContainer, {}, void 0, _jsx('img', {
+  src: '/images/barcode.png',
+  alt: 'barcode'
+})), _jsx(PrintLink, {
+  onClick: print
+}, void 0, 'Print')), _jsx('div', {
+  className: 'token'
+}, void 0, _jsx('div', {
+  className: 'logo'
+}, void 0, _jsx('img', {
+  src: '/images/logo.svg',
+  alt: 'logo'
+})), _jsx('p', {}, void 0, 'Congratulations!! You have beaten our Tomato Trader Challenge. Head over to the Ben & Jerry\u2019s stall for a small peaceful reward with your ice cream and hopefully now you\u2019ll find it easier to negotiate the next challenge you face, like the strong women in the Democratic Republic of Congo'), _jsx(ImageContainer, {}, void 0, _jsx('img', {
+  src: '/images/barcode.png',
+  alt: 'barcode'
+}))));
+
+function Print() {
+
+  return _ref;
+}
+
+/* harmony default export */ exports["default"] = Print;
+
+/***/ },
+
+/***/ "./app/components/Print/styles.css":
+/***/ function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__("./node_modules/css-loader/index.js!./app/components/Print/styles.css");
+if(typeof content === 'string') content = [[module.i, content, '']];
+// add the styles to the DOM
+var update = __webpack_require__("./node_modules/style-loader/addStyles.js")(content, {});
+if(content.locals) module.exports = content.locals;
+// Hot Module Replacement
+if(false) {
+	// When the styles change, update the <style> tags
+	if(!content.locals) {
+		module.hot.accept("!!../../../node_modules/css-loader/index.js!./styles.css", function() {
+			var newContent = require("!!../../../node_modules/css-loader/index.js!./styles.css");
+			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+			update(newContent);
+		});
+	}
+	// When the module is disposed, remove the <style> tags
+	module.hot.dispose(function() { update(); });
+}
+
+/***/ },
+
+/***/ "./node_modules/css-loader/index.js!./app/components/Print/styles.css":
+/***/ function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("./node_modules/css-loader/lib/css-base.js")();
+// imports
+
+
+// module
+exports.push([module.i, "\n.token {\n\ttext-align: center;\t\n\tpadding:8px;\n\tdisplay: none;\n}\n\nimg {\n\tmax-width: 90%;\n}\n\n\n@media print {\n  .no-print {\n    display: none;\n\t}\n\t.token {\n\t\tdisplay: block;\n\t}\n}", ""]);
+
+// exports
+
+
+/***/ }
+
+});
--- build/5.95a34fa918c79f80e5e5.chunk.js
+++ build/5.95a34fa918c79f80e5e5.chunk.js
@@ -1,1 +1,287 @@
-webpackJsonp([5],{"./app/containers/HomePage/constants.js":function(a,b,c){"use strict";c.d(b,"a",function(){return d}),c.d(b,"e",function(){return e}),c.d(b,"f",function(){return f}),c.d(b,"g",function(){return g}),c.d(b,"b",function(){return h}),c.d(b,"c",function(){return i}),c.d(b,"d",function(){return j}),c.d(b,"j",function(){return k}),c.d(b,"h",function(){return l}),c.d(b,"i",function(){return m});var d="app/Home/SCRIPT_FETCH_REQUESTED",e="app/Home/SCRIPT_FETCH_SUCCEEDED",f="app/Home/SCRIPT_FETCH_FAILED",g="borderbot/Home/RESPOND",h="app/Home/TIMER_START",i="app/Home/TIMER_TICK",j="app/Home/PRINT",k="app/Home/BOT_MESSAGE_INVISIBLE",l="app/Home/BOT_MESSAGE_WRITING",m="app/Home/BOT_MESSAGE_VISIBLE"},"./app/containers/HomePage/reducer.js":function(a,b,c){"use strict";function d(a,b){var c=b.getIn(["responses"]);if(void 0==c)return console.log("// no immediate responses"),a;var d=a.update("feed",function(a){var b=a;return c.toArray().forEach(function(a){var c={speaker:1,status:i.j,type:a.get("type"),content:a.get("content"),more:a.get("more"),scoreChange:a.get("scoreChange")};b=b.push(c)}),b});return d}function e(a,b){var c=a.getIn(["script","stages",b,"prompts"]),d=a.update("feed",function(a){var b=a;return c.toArray().forEach(function(a){var c={speaker:-1,status:i.j,type:a.get("type"),content:a.get("content"),more:a.get("more"),scoreChange:a.get("scoreChange")};b=b.push(c),a.get("more")&&(console.log("***"+c.more),console.log(b.toJS()))}),b});return d}function f(a,b){var d=Object.assign({},b.toJS()),e=a.toJS();for(var f in e){var h=e[f];switch(h.operator){case"multiply":d[f]=Math.ceil(d[f]*h.amount);break;case"add":d[f]+=h.amount;break;case"set":d[f]=h.amount;break;default:throw"Unknown change operator "+h.operator;}}return c.i(g.fromJS)(d)}Object.defineProperty(b,"__esModule",{value:!0});var g=c("./node_modules/immutable/dist/immutable.js"),h=c.n(g),i=c("./app/containers/HomePage/constants.js"),j=c.i(g.fromJS)({script_loaded:!1,current_stage_id:!1,show_user_choices:!1,script:{},feed:[],scores:{}});b["default"]=function(){var a=0<arguments.length&&void 0!==arguments[0]?arguments[0]:j,b=arguments[1];switch(b.type){case i.e:var c=a.merge({script_loaded:!0,script:b.script,current_stage_id:b.script.setup.initial_stage_id,scores:b.script.setup.initial_scores});return c=e(c,b.script.setup.initial_stage_id),c;case i.f:return console.log("script fetch failed"),a;case i.g:var g=a.getIn(["script","stages",b.stage_id,"choices",b.choice_id]);if("route"===g.getIn(["targetType"]))return window.location.href=g.getIn(["target"]),a;var h=g.getIn(["target"]),c=a.set("current_stage_id",h).set("show_user_choices",!1).update("feed",function(a){return a.push({speaker:0,type:"text",content:g.get("text")})});return c=d(c,g),c=e(c,h),c;case i.c:var k=a.get("feed").findIndex(function(a){return(-1==a.speaker||1==a.speaker)&&a.status==i.h});if(-1<k){var l=a.updateIn(["feed",k],function(a){var b=Object.assign({},a);return b.status=i.i,b}),m=a.getIn(["feed",k]).scoreChange;return m&&(l=l.updateIn(["scores"],function(a){return f(m,a)})),l}var n=a.get("feed").findIndex(function(a){return(-1==a.speaker||1==a.speaker)&&a.status==i.j});return-1<n?a.updateIn(["feed",n],function(a){var b=Object.assign({},a);return b.status=i.h,b}):!1==a.get("show_user_choices")&&!1!==a.get("current_stage_id")?a.set("show_user_choices",!0):a;case i.d:window.print();break;default:return a;}}}});
+webpackJsonp([5],{
+
+/***/ "./app/containers/HomePage/constants.js":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return SCRIPT_FETCH_REQUESTED; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "e", function() { return SCRIPT_FETCH_SUCCEEDED; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "f", function() { return SCRIPT_FETCH_FAILED; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "g", function() { return RESPOND; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "b", function() { return TIMER_START; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "c", function() { return TIMER_TICK; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "d", function() { return PRINT; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "j", function() { return BOT_MESSAGE_INVISIBLE; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "h", function() { return BOT_MESSAGE_WRITING; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "i", function() { return BOT_MESSAGE_VISIBLE; });
+/*
+ *
+ * Home page constants
+ *
+ */
+
+var SCRIPT_FETCH_REQUESTED = 'app/Home/SCRIPT_FETCH_REQUESTED';
+var SCRIPT_FETCH_SUCCEEDED = 'app/Home/SCRIPT_FETCH_SUCCEEDED';
+var SCRIPT_FETCH_FAILED = 'app/Home/SCRIPT_FETCH_FAILED';
+
+var RESPOND = 'borderbot/Home/RESPOND';
+
+var TIMER_START = 'app/Home/TIMER_START';
+var TIMER_TICK = 'app/Home/TIMER_TICK';
+var PRINT = 'app/Home/PRINT';
+
+var BOT_MESSAGE_INVISIBLE = 'app/Home/BOT_MESSAGE_INVISIBLE';
+var BOT_MESSAGE_WRITING = 'app/Home/BOT_MESSAGE_WRITING';
+var BOT_MESSAGE_VISIBLE = 'app/Home/BOT_MESSAGE_VISIBLE';
+
+/***/ },
+
+/***/ "./app/containers/HomePage/reducer.js":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_immutable__ = __webpack_require__("./node_modules/immutable/dist/immutable.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_immutable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_immutable__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__constants__ = __webpack_require__("./app/containers/HomePage/constants.js");
+/*
+ *
+ * Gimp reducer
+ *
+ */
+
+
+
+
+var initialState = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0_immutable__["fromJS"])({
+  script_loaded: false,
+  current_stage_id: false,
+  show_user_choices: false,
+  script: {},
+  feed: [],
+  scores: {}
+});
+
+function homeReducer() {
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : initialState;
+  var action = arguments[1];
+
+
+  switch (action.type) {
+    case __WEBPACK_IMPORTED_MODULE_1__constants__["e" /* SCRIPT_FETCH_SUCCEEDED */]:
+
+      var _nextState = state.merge({
+        script_loaded: true,
+        script: action.script,
+        current_stage_id: action.script.setup.initial_stage_id,
+        scores: action.script.setup.initial_scores
+      });
+
+      _nextState = addPromptMessages(_nextState, action.script.setup.initial_stage_id);
+
+      return _nextState;
+
+    case __WEBPACK_IMPORTED_MODULE_1__constants__["f" /* SCRIPT_FETCH_FAILED */]:
+
+      console.log('script fetch failed');
+      return state;
+
+    case __WEBPACK_IMPORTED_MODULE_1__constants__["g" /* RESPOND */]:
+
+      // get current and next place in script
+      var userResponse = state.getIn(['script', 'stages', action.stage_id, 'choices', action.choice_id]);
+
+      if (userResponse.getIn(['targetType']) === 'route') {
+        // just abort everything and change the location
+        window.location.href = userResponse.getIn(['target']);
+        return state;
+      }
+      // otherwise, assume that this was a response that requires a
+      // modification to the feed
+
+      // assume targetType === 'feed'
+      var next_stage_id = userResponse.getIn(['target']);
+
+      var _nextState = state
+
+      // update current place in script
+      .set('current_stage_id', next_stage_id)
+
+      // hide user choices
+      .set('show_user_choices', false)
+
+      // add user's response
+      .update('feed', function (arr) {
+        return arr.push({
+          speaker: 0,
+          type: "text",
+          content: userResponse.get('text')
+        });
+      });
+
+      // set current context
+      // .set('current_context', currentContext)
+
+
+      // add bot's immediate response (0+ messages)
+      _nextState = addImmediateChoiceResponses(_nextState, userResponse);
+
+      // add bot's prompts (0+ messages )
+      _nextState = addPromptMessages(_nextState, next_stage_id);
+
+      return _nextState;
+
+    case __WEBPACK_IMPORTED_MODULE_1__constants__["c" /* TIMER_TICK */]:
+
+      // if none, find first element in feed with status=BOT_MESSAGE_WRITING, set to BOT_MESSAGE_VISIBLE
+      var firstWritingMessageIndex = state.get('feed').findIndex(function (message) {
+        return (message.speaker == -1 || message.speaker == 1) && message.status == __WEBPACK_IMPORTED_MODULE_1__constants__["h" /* BOT_MESSAGE_WRITING */];
+      });
+
+      if (firstWritingMessageIndex > -1) {
+
+        // update status of message
+        var updatedFeed = state.updateIn(['feed', firstWritingMessageIndex], function (message) {
+          var newMessage = Object.assign({}, message);
+          newMessage.status = __WEBPACK_IMPORTED_MODULE_1__constants__["i" /* BOT_MESSAGE_VISIBLE */];
+          return newMessage;
+        });
+
+        // get score change if any
+        var scoreChange = state.getIn(['feed', firstWritingMessageIndex]).scoreChange;
+
+        if (scoreChange) {
+          // update score
+          updatedFeed = updatedFeed.updateIn(['scores'], function (scores) {
+            return calculateNewScores(scoreChange, scores);
+          });
+        } else {}
+
+        return updatedFeed;
+      }
+
+      // find first element in feed with status=BOT_MESSAGE_INVISIBLE, set to BOT_MESSAGE_WRITING
+      var firstInvisibleMessageIndex = state.get('feed').findIndex(function (message) {
+        return (message.speaker == -1 || message.speaker == 1) && message.status == __WEBPACK_IMPORTED_MODULE_1__constants__["j" /* BOT_MESSAGE_INVISIBLE */];
+      });
+      if (firstInvisibleMessageIndex > -1) {
+        return state.updateIn(['feed', firstInvisibleMessageIndex], function (message) {
+          var newMessage = Object.assign({}, message);
+          newMessage.status = __WEBPACK_IMPORTED_MODULE_1__constants__["h" /* BOT_MESSAGE_WRITING */];
+          return newMessage;
+        });
+      }
+
+      // // if none, show user choices if hidden and there are choices available
+      if (state.get('show_user_choices') == false && state.get('current_stage_id') !== false) {
+        return state.set('show_user_choices', true);
+      }
+
+      return state;
+
+    case __WEBPACK_IMPORTED_MODULE_1__constants__["d" /* PRINT */]:
+      window.print();
+      break;
+
+    default:
+      return state;
+  }
+}
+
+// this adds immediate responses to a given choice
+function addImmediateChoiceResponses(state, userResponse) {
+
+  var immediateResponses = userResponse.getIn(['responses']);
+
+  if (immediateResponses == undefined) {
+    // no immediate responses
+    console.log('// no immediate responses');
+    return state;
+  }
+
+  var nextState = state.update('feed', function (arr) {
+
+    var newArr = arr;
+    immediateResponses.toArray().forEach(function (response) {
+      var feedMessage = {
+        speaker: 1, // bot response code, TODO factor out as CONST
+        status: __WEBPACK_IMPORTED_MODULE_1__constants__["j" /* BOT_MESSAGE_INVISIBLE */],
+        // TODO: do something with prompt.type, contentType?
+        type: response.get('type'),
+        content: response.get('content'),
+        more: response.get('more'),
+        scoreChange: response.get('scoreChange')
+      };
+
+      newArr = newArr.push(feedMessage);
+    });
+
+    return newArr;
+  });
+
+  return nextState;
+}
+
+// this adds prompt messages for a given stage
+function addPromptMessages(state, stage_id) {
+
+  var prompts = state.getIn(['script', 'stages', stage_id, 'prompts']);
+
+  var nextState = state.update('feed', function (arr) {
+
+    var newArr = arr;
+    prompts.toArray().forEach(function (prompt) {
+
+      var feedMessage = {
+        speaker: -1, // narrator response code, TODO factor out as CONST
+        status: __WEBPACK_IMPORTED_MODULE_1__constants__["j" /* BOT_MESSAGE_INVISIBLE */],
+        // TODO: do something with prompt.type, contentType?
+        type: prompt.get('type'),
+        content: prompt.get('content'),
+        more: prompt.get('more'),
+        scoreChange: prompt.get('scoreChange')
+      };
+
+      newArr = newArr.push(feedMessage);
+
+      if (prompt.get('more')) {
+        console.log('***' + feedMessage.more);
+        console.log(newArr.toJS());
+      }
+    });
+
+    return newArr;
+  });
+
+  return nextState;
+}
+
+function calculateNewScores(scoreChange, currentScores) {
+  var newScores = Object.assign({}, currentScores.toJS());
+  var scoreChangeJS = scoreChange.toJS();
+
+  for (var scoreName in scoreChangeJS) {
+    var change = scoreChangeJS[scoreName];
+
+    switch (change.operator) {
+      case 'multiply':
+        newScores[scoreName] = Math.ceil(newScores[scoreName] * change.amount);
+        break;
+      case 'add':
+        newScores[scoreName] += change.amount;
+        break;
+      case 'set':
+        newScores[scoreName] = change.amount;
+        break;
+      default:
+        throw 'Unknown change operator ' + change.operator;
+    }
+  }
+  return __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0_immutable__["fromJS"])(newScores);
+}
+
+/* harmony default export */ exports["default"] = homeReducer;
+
+/***/ }
+
+});
